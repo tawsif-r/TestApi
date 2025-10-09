e425f722b49a21b8894059fe29dbb9d76d665333
tawsif-r <urrahmantawsif@gmail.com>
2025-10-09 13:25:00 +0600

    added logger in office

--- Controllers/OfficesController.cs
+++ Controllers/OfficesController.cs
@@ -12,17 +12,40 @@ namespace TestApi.Controllers
     public class OfficesController : ControllerBase
     {
         private readonly ApplicationDbContext dbContext;
+        private readonly ILogger<OfficesController> logger;
+
         // make constructor
-        public OfficesController(ApplicationDbContext dbContext)
+        public OfficesController(ApplicationDbContext dbContext,ILogger<OfficesController> logger)
         {
             this.dbContext = dbContext;
-
+            this.logger = logger;
         }
         [HttpGet]
         public IActionResult GetAllOffices()
         {
             var allOffices = dbContext.Offices.ToList();
             return Ok(allOffices);
+        }
+
+        //POST REQUEST
+        [HttpPost]
+        public async Task<IActionResult> CreateOffice([FromBody] Office office)
+        {
+            try
+            {
+                dbContext.Add(office);
+                await dbContext.SaveChangesAsync();
+                return Ok(new { message = "office successfully created", Data = office });
+
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, $"Error occured while saving the office with id:{office.Id}");
+                return StatusCode(500, new
+                {
+                    Message = "An unexpected error had occured"
+                });
+            }
         }
     }
 }
--- obj/Debug/net9.0/TestApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/TestApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d907afc9f6f270745bfc651113123c76c606d773")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47b079025b445750e98b40a0cf35de6628bfbce1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
